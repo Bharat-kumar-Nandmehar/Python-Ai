657f0a13d7dee7aab3805a336ea4711b2d2a6780
Bharat kumar Nandmehar <bk29bharatkumar@gmail.com>
2022-02-11 01:10:14 +0530

    addes list.py

---
+++ lists.py
@@ -0,0 +1,15 @@
+#List
+#a=[1,2,3,4,23,56,34,12,50,13]
+#a[0]=300 #replace the number at index 0
+#a.sort() sort the list
+#a.reverse() #reverse the list
+#a.append(12) # add the number at the last of list
+#a.insert(2,544) #insert the number 544 at index 2
+#a.pop(2) # removes the number at index 2
+#a.remove(3) #removes the number 3 in list
+'''b=a[0]+a[1]+a[2]+a[3]
+print(b)'''
+#print(a)
+
+
+
