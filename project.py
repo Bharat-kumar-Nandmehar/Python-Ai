537ee46c545975e18cd1e22878a85983b3b13099
Bharat kumar Nandmehar <bk29bharatkumar@gmail.com>
2022-02-16 01:58:59 +0530

    Add some methos to project.py

--- project.py
+++ project.py
@@ -1,5 +1,6 @@
 #This project is of hotel managemnet
 print("Welcome to our Hotel")
+print("Press 1:- To Book Double bed A/C room")
 ch=int(input("Enter your choice: "))
 if ch==1:
     print("Facilities in Room")
@@ -7,9 +8,6 @@ if ch==1:
     print("",D_A_R)
     book=input("To book this room press yes (y): ")
     if book=="yes" or book=="Yes" or book=="y" or book=="Y":
-        '''def Detailsofcustomer(name,email,phone,address):
-            customer=Detailsofcustomer
-            return customer'''
         print("Rooms available in this hotel for this category is '20' ")
         no_rm_book=int(input("Enter no. of people: "))
         name=input("Enter your name: ")
@@ -17,41 +15,69 @@ if ch==1:
         phone=input("Enter your mobile (number): ")
         address=input("Enter your address: ")
         rm=int(input("Enter No. of rooms you want to register: "))
-        #for int in rm:
         avail=20-rm
         print("No. of room availbale now: ",avail)
         print("If you want to change your details press(c) otherwise no further changes will be made \n'If you want to continue press(y)'")
         update = input("To change press(c), continue press(y): ")
-        #re_up = "y"
-        #while re_up == "yes" or re_up == "Yes" or re_up == "y" or re_up == "Y":
-        #update = input("To change press(c), continue press(y): ")
-
         if update=='c' or update=='C':
-
+            #re='c'
+            #while re=='c' or re=='C':
                 up=[name,email,phone,address]
-            #print("",up)
                 print("'To change name press(n)'\n 'To change email press(e)'\n 'To change phone press(p)'\n 'To change address press(a)'")
-          #  re='y'
-            #while re== "yes" or re== "Yes" or re== "y" or re== "Y":
                 change=input("To change your details: ")
                 if change=='n' or change=='N':
                     up[0]=input("New name: ")
+                    up=[up[0],email,phone,address]
+                    print("New Details: ",up)
+                    enter=input("Press enter to update details/to print your receipt: ")
+                    if enter=='y' or enter=='Y':
+                        print("Thank you for booking your Room")
+                        print("Your Details: ", up)
+                        print("Your OTP for verification oy your id: ", up[0][0:2] + up[2][4:8])
                 elif change=='e' or change=='E':
                     up[1]=input("New Email: ")
+                    up = [name, up[1], phone, address]
+                    print("New Details: ", up)
+                    enter = input("Press enter to update details/to print your receipt: ")
+                    if enter == 'y' or enter == 'Y':
+                        print("Thank you for booking your Room")
+                        print("Your Details: ", up)
+                        print("Your OTP for verification oy your id: ", up[0][0:2] + up[2][4:8])
                 elif change=='p' or change=='P':
                     up[2]=input("New phone no.: ")
+                    up = [name, email,up[2] , address]
+                    print("New Details: ", up)
+                    enter = input("Press enter to update details/to print your receipt: ")
+                    if enter == 'y' or enter == 'Y':
+                        print("Thank you for booking your Room")
+                        print("Your Details: ", up)
+                        print("Your OTP for verification oy your id: ", up[0][0:2] + up[2][4:8])
                 elif change=='a' or change=='A':
                     up[3]=input("New address: ")
+                    up = [name, email, phone, up[3]]
+                    print("New Details: ", up)
+                    enter = input("Press enter to update details/to print your receipt: ")
+                    if enter == 'y' or enter == 'Y':
+                        print("Thank you for booking your Room")
+                        print("Your Details: ", up)
+                        print("Your OTP for verification oy your id: ", up[0][0:2] + up[2][4:8])
                 else:
                     print("Wrong choice")
-                print("",up)
-                   # re=input("To make changes again press(y): ")
+        elif update=='y' or update=='Y':
+            up=(name,email,phone,address)
+            print("Thank you for booking your Room")
+            print("Your Details: ",up)
+            print("Your OTP for verifiation oy your id: ", name[0] + name[1] + phone[3:8])
 
 
 
 
-        #fun=Detailsofcustomer(name,email,phone,address)
-       # print("",fun)
+
+
+
+
+
+
 
 
 
