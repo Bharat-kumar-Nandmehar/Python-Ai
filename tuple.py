4f060ea751cf531d27a7e2b0160b54069d663294
Bharat kumar Nandmehar <bk29bharatkumar@gmail.com>
2022-02-17 20:14:27 +0530

    Made tuples.py , Modified project.py

---
+++ tuple.py
@@ -0,0 +1,27 @@
+#This is for tuples
+#a=(1,2,3,1,434,34,1,2242,1,343,12)
+#b=sorted(a) #This will sort the tuple
+#print("",b)
+#sorted(a) #This will also sort the tuple
+#print("",a)
+#b=min(a) #This will give the minimum value of tuple a
+#print("",b)
+#b=max(a) #This will give you the maximum value of tuple a
+#print("",b)
+#b=sum(a) #This will give you the sum of all value of tuple a (but in nested tuple it throws error)
+#print("",b)
+#b=a.index(2) #This will tell at index 1 , 2 is present
+#print("",b)
+#b=a.count(1) #This is will tell you that how many times 1 come in this tuple
+#print("",b)
+#Tuple to list then list to tuple
+'''a=(1,2,"Good","Hello")
+print("",a)
+print("",type(a))
+lst=list(a)
+lst[1]=3000
+print("",lst)
+print("",type(lst))
+a=tuple(lst)
+print("",a)
+print("",type(a))'''
