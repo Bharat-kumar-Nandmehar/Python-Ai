6a856a13c7c15765672d52d55f5dab03ea2820a1
Bharat kumar Nandmehar <bk29bharatkumar@gmail.com>
2022-02-17 19:14:14 +0530

    modidfied list

--- lists.py
+++ lists.py
@@ -10,6 +10,27 @@
 '''b=a[0]+a[1]+a[2]+a[3]
 print(b)'''
 #print(a)
-
-
+#a.extend([2,3,4,11]) #Extend the list
+#del a[0] #Delete the item at index 0
+#del a[0:2] #Delete the item at index 0 to 1
+#a=[] #This gives a empty list
+#a=list("This is a list") #In this the  string is converted into a list
+#a=[2,34,23,213,546]
+#min(a)
+#print("",min(a))#This will print the minimum value of items in list a
+#print("",max(a))#This will print the maximum value of items in list a
+#print("",sum(a))#This will print the sum of all value of items in list a
+#eval=eval (input("Enter the number: ")) #This will cast it as a integer
+#print("",eval)
+#Tuple to list then list to tuple
+'''a=(1,2,"Good","Hello")
+print("",a)
+print("",type(a))
+lst=list(a)
+lst[1]=3000
+print("",lst)
+print("",type(lst))
+a=tuple(lst)
+print("",a)
+print("",type(a))'''
 
